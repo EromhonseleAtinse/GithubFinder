001ca8553ac904a06404409cf39a9f7d0ba0218e
atinse <atinseeromhonsele@gmail.com>
2020-04-04 13:43:25 +0100

    added build

---
+++ build/precache-manifest.d8d0fcb83adfbc1ac0a6f599db627db8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03fd8ed5749f2b63cfd2963c7711e2b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a94e86663046115f81fb",
+    "url": "/static/css/main.b4f77495.chunk.css"
+  },
+  {
+    "revision": "b5e03ea2cc9e0632f8a9",
+    "url": "/static/js/2.dbfaf0b1.chunk.js"
+  },
+  {
+    "revision": "a94e86663046115f81fb",
+    "url": "/static/js/main.f119eb51.chunk.js"
+  },
+  {
+    "revision": "24fccac137a83a0ea4fc",
+    "url": "/static/js/runtime-main.860e89cd.js"
+  }
+]);
